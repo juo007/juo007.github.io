eca4cc0eac41b620db65cdc7db9c35d38284e152
juo007 <juo007@ucsd.edu>
2016-09-30 18:33:15 -0700

    first iteration

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,16 @@
 $(document).ready(function(){
-	var page = $("html, body");
 	
 	$(".nav a").click( function(){
 		$(".nav").find(".active").removeClass("active");
 		$(this).parent().addClass("active");
 	});
 	
-	$(".views, .next").click(function() {
-		
-		/*
-		page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
-		page.stop();
-		});
-		
-		page.animate({ scrollTop: $(this).height()/4 }, 'slow', function(){
-		page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
-		});
-		*/
-
-		$("html, body").stop(true,true).animate({ scrollTo: $(document).height()/4 }, "fast");
-				
+	$(".views, .button").click(function() {
+		$("html, body").stop(true,true).animate({ scrollTop: $(document).height()/4 }, "slow");
 	});
 	
 	$("#contact").click(function() {
-		$("html, body").stop(true,true).animate({ scrollTop: $(document).height() }, "fast");
+		$("html, body").stop(true,true).animate({ scrollTop: $(document).height() }, "slow");
 	});
 	
 });
