10a4e0ff53faf9366b4b92027305134f9141a7e4
juo007 <juo007@ucsd.edu>
2016-09-30 18:29:26 -0700

    new aboutme

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
 $(document).ready(function(){
+	var page = $("html, body");
 	
 	$(".nav a").click( function(){
 		$(".nav").find(".active").removeClass("active");
 		$(this).parent().addClass("active");
 	});
 	
-	$(".views, .button").click(function() {
-		$("html, body").animate({ scrollTop: $(document).height()/4 }, "fast");
+	$(".views, .next").click(function() {
+		
+		/*
+		page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+		page.stop();
+		});
+		
+		page.animate({ scrollTop: $(this).height()/4 }, 'slow', function(){
+		page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+		});
+		*/
+
+		$("html, body").stop(true,true).animate({ scrollTo: $(document).height()/4 }, "fast");
+				
 	});
 	
 	$("#contact").click(function() {
-		$("html, body").animate({ scrollTop: $(document).height() }, "fast");
+		$("html, body").stop(true,true).animate({ scrollTop: $(document).height() }, "fast");
 	});
 	
 });
